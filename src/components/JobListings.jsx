1f8024774505363ef4b1c976eb9c561b7f47a8c5
kristinamakarin <makarin.kristina@yahoo.com>
2026-06-15 11:36:51 +0200

    Feature: Integrated JSON Server as backend API and fetched dynamic data using useState and useEffect hooks

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,16 +1,42 @@
 import jobs from '../jobs.json';
 import JobListing from './JobListing';
+import { useState, useEffect } from 'react';
 import React from 'react'
 
-const JobListings = () => {
-    const recentJobs = jobs.slice(0, 3);
+const JobListings = ( {isHome = false} ) => {
+    const [jobs, setJobs] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const res = await fetch('http://localhost:8000/jobs');
+        const data = await res.json();
+        setJobs(data);
+      } catch (error) {
+        console.log('Error fetching data', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchJobs();
+  }, []);
+
+
   return (
     <section className="cards-section-jobs">
-        <h2 className="listings-title">Browse Jobs</h2>
+        <h2 className="listings-title"> { isHome ? 'Recent Jobs' : 'Browse Jobs' }</h2>
         <div className="cards-grid-jobs">
-            {recentJobs.map((job) => (
-                <JobListing key={job.id} job={job} />
-            ))}
+            {loading ? (
+                <h2>Loading...</h2>
+            ) : (
+                <>
+                    {jobs.map((job) => (
+                        <JobListing key={job.id} job={job} />
+                    ))}
+                </>
+            )}
         </div>
     </section>
   )
